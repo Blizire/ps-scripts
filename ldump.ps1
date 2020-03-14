e366a6b558bf0e913d4e54e73d830c9b4009747e
Trenton Stiles <Stiles.Trenton@gmail.com>
2020-03-14 13:20:43 -0600

    Add files via upload

---
+++ ldump.ps1
@@ -0,0 +1,35 @@
+﻿# name : ldump.ps1
+# author : trenton stiles
+# description : monitors what you have in clipboard ( text you copy )
+#               then dumps that text into a text file everytime you
+#               you copy something new                
+
+param(
+    [string] $path 
+)
+
+# $path is location ldump text will be generated too
+
+if( !$path )
+{
+    $path = "~/Desktop/ldump.txt";
+}
+
+$placeHolder = (get-clipboard);
+
+while($true)
+{
+    $clipboard = (Get-Clipboard);
+    if($clipboard -ne $placeHolder)
+    {
+        Write-Host $clipboard
+        $placeHolder = $clipboard;
+        echo $clipboard >> $path;
+
+    }
+
+    if((get-clipboard) -eq $placeHolder)
+    {
+        sleep -Milliseconds 200
+    }
+}
